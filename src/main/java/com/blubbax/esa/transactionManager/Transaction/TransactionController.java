6ec9f3f43da41e256996552ff421cc65e99aa045
Nico Pistor <nico.pistor@hs-furtwangen.de>
2022-06-15 12:26:46 +0200

    Hateoas

--- src/main/java/com/blubbax/esa/transactionManager/Transaction/TransactionController.java
+++ src/main/java/com/blubbax/esa/transactionManager/Transaction/TransactionController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.availability.AvailabilityChangeEvent;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
@@ -47,15 +48,46 @@ public class TransactionController {
     }
 
     @Operation(summary = "Get all transactions")
-    @GetMapping("/api/transaction")
+    @GetMapping(value = "/api/transaction", produces = { "application/hal+json" })
     public List<Transaction> getAllTransactionDatasets() {
-        return transactionService.getAllTransactionDatasets();
+
+        List<Transaction> transactions = transactionService.getAllTransactionDatasets();
+        for(final Transaction transaction: transactions) {
+            transaction.add(linkTo(methodOn(TransactionController.class).getTransactionDataset(transaction.getId())).withSelfRel());
+        }
+
+        return  transactions;
     }
 
     @Operation(summary = "Get all transactions for a user")
-    @GetMapping("/api/transaction/user/{userId}")
-    public List<Transaction> getAllTransactionDatasetsByUser(@PathVariable String userId) {
-        return transactionService.getAllTransactionDatasetsByUser(userId);
+    @GetMapping(value = "/api/transaction/user/{userId}", produces = { "application/hal+json" }, params = { "page", "size" })
+    public CollectionModel<Transaction> getAllTransactionDatasetsByUser(
+            @PathVariable String userId,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+
+        Page<Transaction> transcation_page = transactionService.getAllTransactionDatasetsByUser(userId, page, size);
+
+        if (page >= transcation_page.getTotalPages()) {
+            throw new TransactionNotFoundException(null);
+        }
+
+        List<Transaction> transcations = transcation_page.toList();
+
+        for(final Transaction transaction: transcations) {
+            transaction.add(linkTo(methodOn(TransactionController.class).getTransactionDataset(transaction.getId())).withSelfRel());
+        }
+
+        ArrayList<Link> links = new ArrayList<>();
+        links.add(linkTo(methodOn(TransactionController.class).getAllTransactionDatasetsByUser(userId, 0, size)).withRel("first"));
+        links.add(linkTo(methodOn(TransactionController.class).getAllTransactionDatasetsByUser(userId, transcation_page.getTotalPages(), size)).withRel("last"));
+        if (page + 1 < transcation_page.getTotalPages())
+            links.add(linkTo(methodOn(TransactionController.class).getAllTransactionDatasetsByUser(userId, page + 1, size)).withRel("next"));
+        if (page > 0)
+            links.add(linkTo(methodOn(TransactionController.class).getAllTransactionDatasetsByUser(userId, page - 1, size)).withRel("prev"));
+        CollectionModel<Transaction> result = CollectionModel.of(transcations, links);
+
+        return result;
     }
 
     @Operation(summary = "Get transaction by its id")
@@ -71,33 +103,36 @@ public class TransactionController {
 
         ArrayList<Link> links = new ArrayList<>();
         links.add(linkTo(methodOn(TransactionController.class).getTransactionDataset(transaction.getId())).withSelfRel());
-        links.add(linkTo(methodOn(TransactionController.class).updateTransactionDataset(transaction.getId(), transaction)).withRel("update"));
         RepresentationModel<Transaction> result = (RepresentationModel<Transaction>) RepresentationModel.of(transaction, links);
         return result;
     }
 
-    @Operation(summary = "Save new hydroponic log entry")
+    @Operation(summary = "Save new transaction")
     @PostMapping("/api/transaction")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Transaction successfully created",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Transaction.class))}),
+                    content = { @Content(mediaType = "application/hal+json", schema = @Schema(implementation = Transaction.class))}),
             @ApiResponse(responseCode = "400", description = "Transaction data not valid",
                     content = @Content)
     })
     public Transaction saveTransactionDataset(@RequestBody @Valid Transaction transaction) {
-        return transactionService.saveTransactionDataset(transaction);
+        Transaction new_transaction = transactionService.saveTransactionDataset(transaction);
+        new_transaction.add(linkTo(methodOn(TransactionController.class).getTransactionDataset(new_transaction.getId())).withSelfRel());
+        return new_transaction;
     }
 
     @Operation(summary = "Update transaction by its id")
     @PutMapping("/api/transaction/{id}")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Transaction successfully updated",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Transaction.class))}),
+                    content = { @Content(mediaType = "application/hal+json", schema = @Schema(implementation = Transaction.class))}),
             @ApiResponse(responseCode = "400", description = "Transaction data not valid",
                     content = @Content)
     })
     public Transaction updateTransactionDataset(@PathVariable Long id, @RequestBody @Valid Transaction newTransaction) {
-        return transactionService.updateTransactionDataset(id, newTransaction);
+        Transaction updated_transaction = transactionService.updateTransactionDataset(id, newTransaction);
+        updated_transaction.add(linkTo(methodOn(TransactionController.class).getTransactionDataset(updated_transaction.getId())).withSelfRel());
+        return updated_transaction;
     }
 
     @Operation(summary = "Delete transaction by its id")
